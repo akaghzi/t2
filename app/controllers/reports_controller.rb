13074411d571fc1b88936aef829826c3825da30f
Asim Kaghzi <akaghzi@gmail.com>
2015-03-02 23:09:28 -0700

    added order date

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,7 +2,7 @@ class ReportsController < ApplicationController
 
   def index
     @sumbycustomer = Order.select("customer_id, sum(order_amount) as total").group("customer_id")
-    @sumbytime = Order.select("strftime('%Y-%m',updated_at) as year_month, sum(order_amount) as total").group("year_month")
+    @sumbytime = Order.select("strftime('%Y-%m',ordered_at) as year_month, sum(order_amount) as total").group("year_month")
     @sumbycity = Customer.joins(:orders).select("city, sum(order_amount) as total").group("customer_id")
   end
 

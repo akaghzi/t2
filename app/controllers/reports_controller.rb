304b6abad22ebe57c5b813a730942bd254ee95c6
Asim Kaghzi <akaghzi@gmail.com>
2015-03-05 18:39:12 -0700

    reporting controller added order by total

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,9 @@
 class ReportsController < ApplicationController
 
   def index
-    @sumbycustomer = Order.select("customer_id, sum(order_amount) as total").group("customer_id")
+    @sumbycustomer = Order.select("customer_id, sum(order_amount) as total").group("customer_id").order("total desc")
     @sumbytime = Order.select("strftime('%Y-%m',ordered_at) as year_month, sum(order_amount) as total").group("year_month")
-    @sumbycity = Order.select("name, sum(order_amount) as total").joins(customer: :city).group("name")
+    @sumbycity = Order.select("name, sum(order_amount) as total").joins(customer: :city).group("name").order("total desc")
   end
 
   def sumbycustomer
